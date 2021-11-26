46639416ea3d1812acee5eb5fe7aae1bbbcb1fbb
“[atefeh <[atefeh@buzzjective.com]>
2021-11-26 18:48:19 +0330

    craete a country select box and data boxes

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-combobox
+      v-model="selected"
+      :items="countries"
+      item-text="Country"
+      item-value="ID"
+      label="Select a country"
+      outlined
+      @change="onChange()"
+    ></v-combobox>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "CountrySelect",
+  props: {
+    countries: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      selected: {},
+    };
+  },
+  methods: {
+    onChange() {
+      this.$emit("get-country", this.selected);
+    },
+  },
+};
+</script>
